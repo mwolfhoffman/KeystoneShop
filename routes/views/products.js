dd85a75a1ae7e65fbf8b2d4d17c560ddd7ee86d4
Michael Hoffman <michaelhoffmanj@gmail.com>
2018-01-13 20:13:49 -0700

    products working

--- routes/views/products.js
+++ routes/views/products.js
@@ -2,11 +2,76 @@ var keystone = require('keystone');
 var async = require('async');
 var numeral = require('numeral');
 
-exports = module.exports = function(req, res) {
-	
-	var view = new keystone.View(req, res);
+loadCategory = (req, res, cb) => {
+	if (req.params.category) {
+		keystone.list('ProductCategory').model.findOne({ key: locals.filters.category }).exec(function (err, result) {
+			res.locals.data.category = result;
+			throw err;
+		});
+	} else {
+		return cb();
+	}
+}
+
+loadCategories = (req, res, cb) => {
+	keystone.list('ProductCategory').model.find().where('primary', true).sort('name').populate('categories').exec(function (err, results) {
+
+		if (err) {
+			throw err;
+		}
+		res.locals.data.categories = results;
+		return cb();
+		// Load the counts for each category
+		// async.each(res.locals.data.categories, function (category, next) {
+		// 	keystone.list('Product').model.count().where('categories').in([category.id]).exec(function (err, count) {
+		// 		category.productCount = count;
+		// 		return next(err);
+		// 	});
+
+		// }, function (err) {
+		// 	return next(err);
+		// });
+	});
+
+};
+
+loadProducts = (req, res) => {
+	var q = keystone.list('Product').paginate({
+		page: req.query.page || 1,
+		perPage: 13,
+		maxPages: 10
+	})
+		.sort('title')
+
+	if (res.locals.data.category) {
+		var inCategories = [res.locals.data.category]
+
+		for (var i = 0; i < res.locals.data.category.categories.length; i++) {
+			var subCategory = res.locals.data.category.categories[i]
+
+			inCategories.push(subCategory)
+		}
+
+		q.where('categories').in(inCategories);
+	}
+
+	q.exec(function (err, results) {
+		console.log('Got results')
+		console.log(results)
+
+		res.locals.data.products = results;
+		if (err) {
+			return next(err);
+		} else {
+			return res.send(res.locals.data)
+		}
+	});
+
+};
+
+exports = module.exports = function (req, res) {
 	var locals = res.locals;
-	
+
 	// Init locals
 	locals.section = 'store';
 	locals.numeral = numeral;
@@ -17,80 +82,10 @@ exports = module.exports = function(req, res) {
 		products: [],
 		categories: []
 	};
-	
-	// Load all categories
-	view.on('init', function(next) {
-		
-		keystone.list('ProductCategory').model.find().where('primary', true).sort('name').populate('categories').exec(function(err, results) {
-			
-			if (err || !results.length) {
-				return next(err);
-			}
 
-			locals.data.categories = results;
-			
-			// Load the counts for each category
-			async.each(locals.data.categories, function(category, next) {
-				keystone.list('Product').model.count().where('categories').in([category.id]).exec(function(err, count) {
-					category.productCount = count;
-					next(err);
-				});
-				
-			}, function(err) {
-				next(err);
-			});
-			
-		});
-		
-	});
-
-	// Load the current category filter
-	view.on('init', function(next) {
-		if (req.params.category) {
-			keystone.list('ProductCategory').model.findOne({ key: locals.filters.category }).exec(function(err, result) {
-				locals.data.category = result;
-				next(err);
-			});
-		} else {
-			next();
-		}
-		
-	});
-
-	// Load the products
-	view.on('init', function(next) {
-		
-		var q = keystone.list('Product').paginate({
-				page: req.query.page || 1,
-				perPage: 13,
-				maxPages: 10
-			})
-			.sort('title')
-			// ≈;
-		
-		if (locals.data.category) {
-			var inCategories = [locals.data.category]
-
-			for (var i = 0; i < locals.data.category.categories.length; i++) {
-				var subCategory = locals.data.category.categories[i]
-
-				inCategories.push(subCategory)
-			}
-
-			q.where('categories').in(inCategories);
-		}
-		
-		q.exec(function(err, results) {
-			console.log('Got results')
-			console.log(results)
-			
-			locals.data.products = results;
-			next(err);
-		});
-		
-	});
-	
-	// Render the view
-	view.render('products');
-	
+	loadCategories(req, res, () => {
+		loadCategory(req, res, () => {
+			loadProducts(req, res);
+		})
+	})
 };
