02e0372c6b9ed1ae17def45432d914e9adefcff8
Michael Hoffman <michaelhoffmanj@gmail.com>
2018-01-14 09:23:58 -0700

    events/event endpoint

--- routes/views/event.js
+++ routes/views/event.js
@@ -1,42 +1,29 @@
 var keystone = require('keystone');
-var async = require('async');
 var numeral = require('numeral');
 
 exports = module.exports = function (req, res) {
-
-    var view = new keystone.View(req, res);
     var locals = res.locals;
-
-    // Set locals
     locals.section = 'events';
     locals.filters = {
         event: req.params.event
     };
     locals.data = {
         events: []
-        };
+    };
     locals.numeral = numeral;
 
-    // Load the current product
-    view.on('init', function (next) {
 
-        var q = keystone.list('Event').model.findOne({
-            slug: locals.filters.event
-        })
+    var q = keystone.list('Event').model.findOne({
+        slug: locals.filters.event
+    })
 
-        q.exec(function (err, result) {
-            if (err) {
-                return next(err);
-            } else {
-
-                locals.data.event = result;
-                return next();
-            }
-        });
-
+    q.exec(function (err, result) {
+        if (err) {
+            throw err;
+        } else {
+            locals.data.event = result;
+            return res.send(locals);
+        }
     });
-
-    // Render the view
-    view.render('event');
 
 };
